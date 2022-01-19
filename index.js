443661f625723c5b2f331236647d4adf95f50608
Bartosz T <btrafidlo@yahoo.com>
2022-01-19 11:50:36 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(){
+
+}
+function subtract(){
+    
+}
+function multiply(){
+    
+}
+function divide(){
+    
+}
+function add(a,b){
+    let result = a + b
+    return result;
+}
+function subtract(a, b){
+    let result = a - b
+    return result;
+}
+function multiply(a,b){
+    let result = a * b
+    return result;
+}
+function divide(a,b){
+    let result = a / b
+    return result;
+}
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(n){
+    let result = parseInt(n,10);
+    return result;
+}
+function preserveDecimal(n){
+     let result = parseInt("nonsesne!",10);
+    return result;
+}
+function preserveDecimal(n){
+    let result = parseFloat(n,10);
+   return result;
+}
